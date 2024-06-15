543042f2977120e8f0b33718217c397d1dfa384d
Manuel <mmirallesjove@gmail.com>
2024-06-15 19:37:10 +0200

    Validación de datos de un formulario

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,17 @@ const router = createRouter({
     {
       path: '/contador',
       name: 'contador',
-      component: () => import('../components/Contador.vue')
+      component: () => import('../modules/contador/components/Contador.vue')
     },
     {
       path: '/lista-de-tareas',
-      name: 'ListaDekkñkTareas',
-      component: () => import('../components/ListaDeTareas.vue')
+      name: 'ListaDeTareas',
+      component: () => import('../modules/listaDeTareas/components/ListaDeTareas.vue')
+    },
+    {
+      path: '/registrar',
+      name: 'registrar',
+      component: () => import('../modules/registro/views/RegistrarView.vue')
     }
 
   ]
