fb1fd736e816918177c8d037f039e59addad4c57
Manuel <mmirallesjove@gmail.com>
2024-06-16 11:45:26 +0200

    Propiedades padre-hijo

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="container">
+    <Header :mensaje="texto"></Header>
 
+    <div class="container">
         <SideBar></SideBar>
 
         <div class="content">
+            <input type="texto" v-model="texto">
+            {{ texto }}
             <router-view></router-view>
         </div>
-
     </div>
 </template>
 
 <script setup>
-    import SideBar from '../components/SideBar.vue'
-  
-
+    import SideBar from '../components/SideBar.vue';
+    import Header from '../components/Header.vue';
+    import {ref} from 'vue';
+    const texto = ref('');
 </script>
 
 
